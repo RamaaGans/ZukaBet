d5b4997ea6f02cbd0c70efd57b9788dc4d66423a
RamaaGans <107300568+RamaaGans@users.noreply.github.com>
2022-06-13 12:39:32 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,34 +1,34 @@
 //=============『 Utama 』================== //
-global.owner = ['6282138919347'] 
-global.mods = [] 
-global.prems = [] 
+global.owner = ['6282142108243','62887436223985'] 
+global.mods = ['6282142108243'] 
+global.prems = ['6282142108243'] 
 
 //=============『 Info Owner 』============== //
-global.nameowner = 'Helga Zexs'
-global.numberowner = '6282138919347'
-global.instagram = 'https://instagram.com/helga_store20'
-global.github = 'https://github.com/Hyzerr'
-global.dana = '081398860278'
-global.pulsa = '082138919347'
-global.gopay = '081398860278'
+global.nameowner = 'Ramaaa'
+global.numberowner = '6282142108243'
+global.instagram = 'https://instagram.com/rassya766'
+global.github = 'https://github.com/'
+global.dana = 'GA ADA'
+global.pulsa = '082142108243'
+global.gopay = '082338098038'
 
 //=============『 Info Bot 』=================//
-global.namebot = 'ShiroBot-Md'
+global.namebot = 'R-BOT-Md'
 global.gc = 'https://chat.whatsapp.com/BxrDMvfLeKTHtvAWKHWZbZ'
-global.web = 'https://www.helgastoree.xyz' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
-global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
+global.web = 'https://chat.whatsapp.com/D5FjDCkZrdr32qYCClLyQ9' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
+global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\nPermanen 20.000'
 
 //=======『 Tampilan Dan Lainnya 』============//
 global.fotonya1 = 'https://telegra.ph/file/e4a2f4339da8a32ad20a1.jpg' //ganti jadi foto bot mu
 global.fotonya2 = 'https://telegra.ph/file/8c471d45b6cedad43cf84.jpg' //ini juga ganti 
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
-global.wm = 'Zuka Beta'
+global.wm = '{ BY ITSME RAMA }'
 global.watermark = wm
-global.wm2 = '                     「 Zuka Beta あ⁩ 」'
-global.wm3 = '⫹⫺ Zuka Beta'
-global.htki = '––––––『' 
-global.htka = '』––––––'
+global.wm2 = 'R-BOT'
+global.wm3 = 'R-BOT'
+global.htki = '______|' 
+global.htka = '|______'
 global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.wait = '_*tunggu sedang di proses...*_'
@@ -36,8 +36,8 @@ global.eror = '_*Server Error*_'
 global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
 global.stiker_wait = 'Stiker sedang dibuat'
-global.packname = '2022'
-global.author = '© Helga Zexs'
+global.packname = '2k22'
+global.author = 'RAMAGANS'
 
 //=============『 Apikey 』================== //
 global.APIs = { // API Prefix
